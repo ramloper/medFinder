776dd2978efd51cd3b09b5e10a6224422cac1a4b
wooram <rla7360@gmail.com>
2024-05-29 08:05:22 +0900

    feat : default project

---
+++ src/main/java/com/urban/MedFinder/MedFinderApplication.java
@@ -0,0 +1,13 @@
+package com.urban.MedFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedFinderApplication.class, args);
+	}
+
+}
